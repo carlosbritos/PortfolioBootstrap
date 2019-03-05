aeb8bc99fd15b948786766e811cd7ab44ea6b789
Carlos Britos <carlosbritos_98@hotmail.com>
2019-03-05 07:12:18 -0300

    imagenes optimizadas

--- js/data.js
+++ js/data.js
@@ -1,42 +1,42 @@
 var elementsList = [
 	{
 		"id": 006,
-		"logoImage": "imagenes/Logo-real-beauty.png",
+		"logoImage": "imagenes/Logo-real-beauty-350.png",
 		"nombreLogo": "Real Beauty",
 		"descripcionLogo": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies dictum vestibulum. Sed pulvinar eros sed nibh dictum, eu varius dolor lobortis. Phasellus id nunc ut lacus pulvinar interdum ut eget neque. Sed posuere sem faucibus quam vehicula gravida. Aenean ac enim massa. Nulla sed eros nec ipsum interdum aliquet a ut libero. Nullam varius pulvinar tellus imperdiet condimentum. Morbi egestas quam id ex interdum, nec bibendum magna iaculis. Proin nunc ante, maximus non elit nec, maximus facilisis nibh. In nisl odio, hendrerit ac sem auctor, aliquam ultricies magna. Aenean pretium, lectus nec accumsan placerat, velit urna tempus velit, sed facilisis sem ipsum at quam. Praesent ornare neque quis ante luctus, eu porttitor turpis fringilla. Quisque vitae fermentum diam, eget consequat erat. Maecenas ornare, lacus sed pharetra eleifend, turpis dolor porttitor felis, at mattis lorem ex luctus lacus.",
 	},
 
 	{
 		"id": 005,
-		"logoImage": "imagenes/logo-magic-nails.jpg",
+		"logoImage": "imagenes/logo-magic-nails-350.png",
 		"nombreLogo": "Magic Nails",
 		"descripcionLogo": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies dictum vestibulum. Sed pulvinar eros sed nibh dictum, eu varius dolor lobortis. Phasellus id nunc ut lacus pulvinar interdum ut eget neque. Sed posuere sem faucibus quam vehicula gravida. Aenean ac enim massa. Nulla sed eros nec ipsum interdum aliquet a ut libero. Nullam varius pulvinar tellus imperdiet condimentum. Morbi egestas quam id ex interdum, nec bibendum magna iaculis. Proin nunc ante, maximus non elit nec, maximus facilisis nibh. In nisl odio, hendrerit ac sem auctor, aliquam ultricies magna. Aenean pretium, lectus nec accumsan placerat, velit urna tempus velit, sed facilisis sem ipsum at quam. Praesent ornare neque quis ante luctus, eu porttitor turpis fringilla. Quisque vitae fermentum diam, eget consequat erat. Maecenas ornare, lacus sed pharetra eleifend, turpis dolor porttitor felis, at mattis lorem ex luctus lacus.",
 	},
 	
 	{
 		"id": 00,
-		"logoImage": "imagenes/logotipo-ada.png",
+		"logoImage": "imagenes/logotipo-ada-350.png",
 		"nombreLogo": "Ada",
 		"descripcionLogo": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies dictum vestibulum. Sed pulvinar eros sed nibh dictum, eu varius dolor lobortis. Phasellus id nunc ut lacus pulvinar interdum ut eget neque. Sed posuere sem faucibus quam vehicula gravida. Aenean ac enim massa. Nulla sed eros nec ipsum interdum aliquet a ut libero. Nullam varius pulvinar tellus imperdiet condimentum. Morbi egestas quam id ex interdum, nec bibendum magna iaculis. Proin nunc ante, maximus non elit nec, maximus facilisis nibh. In nisl odio, hendrerit ac sem auctor, aliquam ultricies magna. Aenean pretium, lectus nec accumsan placerat, velit urna tempus velit, sed facilisis sem ipsum at quam. Praesent ornare neque quis ante luctus, eu porttitor turpis fringilla. Quisque vitae fermentum diam, eget consequat erat. Maecenas ornare, lacus sed pharetra eleifend, turpis dolor porttitor felis, at mattis lorem ex luctus lacus.",
 	},
 
 	{
 		"id": 003,
-		"logoImage": "imagenes/logo-bline.jpg",
+		"logoImage": "imagenes/logo-bline-350.png",
 		"nombreLogo": "B.Line",
 		"descripcionLogo": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies dictum vestibulum. Sed pulvinar eros sed nibh dictum, eu varius dolor lobortis. Phasellus id nunc ut lacus pulvinar interdum ut eget neque. Sed posuere sem faucibus quam vehicula gravida. Aenean ac enim massa. Nulla sed eros nec ipsum interdum aliquet a ut libero. Nullam varius pulvinar tellus imperdiet condimentum. Morbi egestas quam id ex interdum, nec bibendum magna iaculis. Proin nunc ante, maximus non elit nec, maximus facilisis nibh. In nisl odio, hendrerit ac sem auctor, aliquam ultricies magna. Aenean pretium, lectus nec accumsan placerat, velit urna tempus velit, sed facilisis sem ipsum at quam. Praesent ornare neque quis ante luctus, eu porttitor turpis fringilla. Quisque vitae fermentum diam, eget consequat erat. Maecenas ornare, lacus sed pharetra eleifend, turpis dolor porttitor felis, at mattis lorem ex luctus lacus.",
 	},
 	
 	{
 		"id": 004,
-		"logoImage": "imagenes/logo-fdlc2.png",
+		"logoImage": "imagenes/logo-fdlc-350.png",
 		"nombreLogo": "Fuera de la Caja",
 		"descripcionLogo": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies dictum vestibulum. Sed pulvinar eros sed nibh dictum, eu varius dolor lobortis. Phasellus id nunc ut lacus pulvinar interdum ut eget neque. Sed posuere sem faucibus quam vehicula gravida. Aenean ac enim massa. Nulla sed eros nec ipsum interdum aliquet a ut libero. Nullam varius pulvinar tellus imperdiet condimentum. Morbi egestas quam id ex interdum, nec bibendum magna iaculis. Proin nunc ante, maximus non elit nec, maximus facilisis nibh. In nisl odio, hendrerit ac sem auctor, aliquam ultricies magna. Aenean pretium, lectus nec accumsan placerat, velit urna tempus velit, sed facilisis sem ipsum at quam. Praesent ornare neque quis ante luctus, eu porttitor turpis fringilla. Quisque vitae fermentum diam, eget consequat erat. Maecenas ornare, lacus sed pharetra eleifend, turpis dolor porttitor felis, at mattis lorem ex luctus lacus.",
 	},
 		
 	{
 		"id": 007,
-		"logoImage": "imagenes/logo-sofia.png",
+		"logoImage": "imagenes/logo-sofia-350.png",
 		"nombreLogo": "Logo Sofia",
 		"descripcionLogo": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies dictum vestibulum. Sed pulvinar eros sed nibh dictum, eu varius dolor lobortis. Phasellus id nunc ut lacus pulvinar interdum ut eget neque. Sed posuere sem faucibus quam vehicula gravida. Aenean ac enim massa. Nulla sed eros nec ipsum interdum aliquet a ut libero. Nullam varius pulvinar tellus imperdiet condimentum. Morbi egestas quam id ex interdum, nec bibendum magna iaculis. Proin nunc ante, maximus non elit nec, maximus facilisis nibh. In nisl odio, hendrerit ac sem auctor, aliquam ultricies magna. Aenean pretium, lectus nec accumsan placerat, velit urna tempus velit, sed facilisis sem ipsum at quam. Praesent ornare neque quis ante luctus, eu porttitor turpis fringilla. Quisque vitae fermentum diam, eget consequat erat. Maecenas ornare, lacus sed pharetra eleifend, turpis dolor porttitor felis, at mattis lorem ex luctus lacus.",
 	},
@@ -50,7 +50,7 @@ var elementsList = [
 
 	{
 		"id": 001,
-		"logoImage": "imagenes/aa.jpg",
+		"logoImage": "imagenes/aa-350.png",
 		"nombreLogo": "Alcoholicos Anonimos",
 		"descripcionLogo": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies dictum vestibulum. Sed pulvinar eros sed nibh dictum, eu varius dolor lobortis. Phasellus id nunc ut lacus pulvinar interdum ut eget neque. Sed posuere sem faucibus quam vehicula gravida. Aenean ac enim massa. Nulla sed eros nec ipsum interdum aliquet a ut libero. Nullam varius pulvinar tellus imperdiet condimentum. Morbi egestas quam id ex interdum, nec bibendum magna iaculis. Proin nunc ante, maximus non elit nec, maximus facilisis nibh. In nisl odio, hendrerit ac sem auctor, aliquam ultricies magna. Aenean pretium, lectus nec accumsan placerat, velit urna tempus velit, sed facilisis sem ipsum at quam. Praesent ornare neque quis ante luctus, eu porttitor turpis fringilla. Quisque vitae fermentum diam, eget consequat erat. Maecenas ornare, lacus sed pharetra eleifend, turpis dolor porttitor felis, at mattis lorem ex luctus lacus.",
 	},
